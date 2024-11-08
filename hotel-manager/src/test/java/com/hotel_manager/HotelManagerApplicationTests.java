5b8133e508d37b52aead53e1128ddd41cc799d72
Sebastian <sebastian.koczotowski@symmetry.pl>
2024-11-08 09:55:16 +0100

    Initial app commit - Spring Initializer.

---
+++ hotel-manager/src/test/java/com/hotel_manager/HotelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
