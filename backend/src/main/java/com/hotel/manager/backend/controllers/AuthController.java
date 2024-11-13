c83bc039a9c3b79635582e646f45e0f82c434727
Sebastian <sebastian.koczotowski@symmetry.pl>
2024-11-13 08:40:44 +0100

    JWT Token - implementacja mechanizmu logowania po stronie backednu.

--- backend/src/main/java/com/hotel/manager/backend/controllers/AuthController.java
+++ backend/src/main/java/com/hotel/manager/backend/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.hotel.manager.backend.controllers;
 
+import com.hotel.manager.backend.config.UserAuthProvider;
 import com.hotel.manager.backend.dto.CredentialsDto;
 import com.hotel.manager.backend.dto.UserDto;
 import com.hotel.manager.backend.services.UserService;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final UserAuthProvider userAuthProvider;
 
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@RequestBody CredentialsDto credentialsDto) {
      UserDto user = userService.login(credentialsDto);
+     user.setToken(userAuthProvider.createToken(user));
      return ResponseEntity.ok(user);
     }
 }
