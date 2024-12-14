1f6f7e82d9dcf78d2c8e1e908f7d30b5038eac58
SebastianKoczotowski <s096679@student.tu.kielce.pl>
2024-12-14 14:04:44 +0100

    Mock komponentu rezerwacje

--- web/src/app/components/reservations/reservations.component.ts
+++ web/src/app/components/reservations/reservations.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PageableDto} from "../../dto/pagination/pageable-dto";
 
 @Component({
   selector: 'app-reservations',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './reservations.component.scss'
 })
 export class ReservationsComponent {
+  page: number = 0;
+  totalRecords: number = 0;
+  pageable: PageableDto = new PageableDto(this.page, 10, null);
+  rezerwacje: any[] = [];
+  isLoading: boolean = true;
 
+  showReservationsDetails(szczegółyRezerwacji: string, param2: { reservation: any }) {
+
+  }
 }
